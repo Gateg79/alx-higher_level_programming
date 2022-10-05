7fab16d14748662abd7a846c511a6972059e4a6d
Gateg79 <edikan.fred@gmail.com>
2022-10-05 04:05:12 -0700

    New

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,7 +6,7 @@ const newDict = {};
 Object.keys(dict).map(function (key) {
   if (!Array.isArray(newDict[dict[key]])) {
     newDict[dict[key]] = [];
-  }
+  };
   newDict[dict[key]].push(key);
 });
 
